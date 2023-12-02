78e7fbfadaa9cfd2c5afd81b7e6ceba0aa85cf45
ashw-ani <ashwanisharma72002@gmail.com>
2023-12-02 18:46:23 +0530

    created tier based conditional fetching for courses

--- src/api/FetchUsers.js
+++ src/api/FetchUsers.js
@@ -1,29 +1,31 @@
-const FetchUsers = async (findUser,type) => {
-    var myHeaders = new Headers();
-    myHeaders.append("api-key", "123456");
-    myHeaders.append("Content-Type", "application/json");
-    
-    var raw = JSON.stringify({
-      "search": findUser,
-      "type":type
-    });
-    
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    const Users = await fetch("https://ascend-server.onrender.com/api/admin/contact/getDetails", requestOptions);
-    //   .then(response => response.text())
-    //   .then(result => console.log(result))
-    //   .catch(error => console.log('error', error));
-    const data = await Users.json();
-    console.log('====================================');
-    console.log(data);
-    console.log('====================================');
-    return data;
+const FetchUsers = async (findUser, type) => {
+  var myHeaders = new Headers();
+  myHeaders.append("api-key", "123456");
+  myHeaders.append("Content-Type", "application/json");
 
-}
+  var raw = JSON.stringify({
+    search: findUser,
+    type: type,
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  const Users = await fetch(
+    "https://ascend-server.onrender.com/api/admin/contact/getDetails",
+    requestOptions
+  );
+  //   .then(response => response.text())
+  //   .then(result => console.log(result))
+  //   .catch(error => console.log('error', error));
+  const data = await Users.json();
+  console.log("====================================");
+  console.log(data);
+  console.log("====================================");
+  return data;
+};
 export default FetchUsers;
