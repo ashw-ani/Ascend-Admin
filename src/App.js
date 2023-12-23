d10646cd2b20a103e3dd6f1642a99bac9994b12c
eternal-will <shuklashashwat2003@gmail.com>
2023-12-23 12:16:08 +0530

    auth context fix

--- src/App.js
+++ src/App.js
@@ -2,46 +2,17 @@ import styles from "./App.module.css";
 import Sidepanel from "./Components/sidepanel/Sidepanel";
 import Body from "./Components/body/Body";
 import Login from "./Components/Login/Login";
-import { useLocation } from "react-router-dom";
 import {
   Redirect,
   Route,
   Switch,
 } from "react-router-dom/cjs/react-router-dom.min";
-import { useEffect, useState } from "react";
 import { useContext } from "react";
 import AuthContext from "./Context/AuthContext";
-import login from "./api/getLogin";
-// import Header from "./Components/body/Header/Header";
 import { PanelContextProvider } from "../src/Context/PanelContext";
-import * as jwt_decode from "jwt-decode";
 
 function App() {
-  const location = useLocation();
   const context = useContext(AuthContext);
-
-  useEffect(() => {
-    let token = localStorage.getItem("token");
-    if (token) {
-      const { exp } = jwt_decode.jwtDecode(token);
-      const expTime = exp * 1000;
-      if (Date.now() >= expTime) {
-        localStorage.removeItem("token");
-        window.location.href = "/login";
-      }
-    }
-  });
-  useEffect(() => {
-    let token = localStorage.getItem("token");
-    if (token) {
-      const { exp } = jwt_decode.jwtDecode(token);
-      const expTime = exp * 1000;
-      if (Date.now() >= expTime) {
-        localStorage.removeItem("token");
-        window.location.href = "/login";
-      }
-    }
-  }, [location]);
 
   return (
     <div className={`${styles.app}  `}>
@@ -57,7 +28,7 @@ function App() {
       )}
       <Switch>
         <Route path="/login" exact>
-          <Login />
+          {!context.loggedIn ? <Login /> : <Redirect to="/" />}
         </Route>
       </Switch>
     </div>
